0dd3c5ed3a834071708a1e40848b03ada5e0dcab
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-05-27 17:29:29 +0500

    Second lesson Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text('Waleed ur Rehman \n waleedrehman31@gmail.com'),
+        appBar: AppBar(
+          title: Center(
+            child: Text("Login"),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login"),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
